aa1bdd7f7a22e1153deca24290346e022012ad76
martinsking <mking@localhost>
2017-01-09 22:06:30 +0000

    silly commit

---
+++ src/sillytest/SillyTest.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sillytest;
+import java.util.Scanner;
+
+/**
+ *
+ * @author mking
+ */
+public class SillyTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a;
+        float b;
+        String s;
+        Scanner in = new Scanner(System.in);
+        
+     System.out.println("Enter a silly string");
+     s = in.nextLine();
+     System.out.println("Your Silly String was "+s);
+     
+        
+    }
+    
+}
